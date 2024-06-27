e28eba3948afbcec74b06dd01b278af29f4cb47a
alpha-asb <153850077+alpha-asb@users.noreply.github.com>
2024-06-27 11:27:34 +0530

    constant space

--- String/String_Rotate_by_2_Places.cpp
+++ String/String_Rotate_by_2_Places.cpp
@@ -5,9 +5,10 @@ class Solution
     //another string by exactly 2 places.
     bool isRotated(string str1, string str2)
     {
-        // Your code here
+        // Approach 1 : O(n) Time and O(n) Space
+        /*
         int n = str1.size();
-        int m = str2.size();
+        int m = str2.size(); 
         if(n!=m)return false;
         if(n<3){
             return str1==str2;
@@ -17,6 +18,25 @@ class Solution
         string anti_clockwise = str1.substr(n-2) + str1.substr(0,n-2);
  
         return str2 == clockwise || str2 == anti_clockwise;
+        */
+        // Approach 2: O(n) Time and O(1) Space 
+        
+        int n = str1.size();
+        bool clockwise = true,anti_clockwise=true;
+        for(int i=0;i<n;i++){
+            if(str1[i] != str2[(i+2)%n]){
+                clockwise = false;
+                break;
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(str1[(i+2)%n] != str2[i]){
+                anti_clockwise = false;
+                break;
+            }
+        }
+        return clockwise || anti_clockwise;
+        
     }
 
 };
